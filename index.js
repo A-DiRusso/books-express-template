6c0dae6df28b210fc26cd732c120e334a6240ddd
Anthony DiRusso <ossurid@gmail.com>
2019-04-03 18:40:58 -0400

    creates skeleton for books-express-templates

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express'),
+    es6Renderer = require('express-es6-template-engine'),
+    app = express();
+const session = require('express-session');
+const FileStore = require('session-file-store')(session);
+
+
+const PORT = 3000;
+
+app.engine('html', es6Renderer);
+app.set('views', 'views');
+app.set('view engine', 'html');
+app.use(express.urlencoded({extended: true}));
+app.use(session({
+    store: new FileStore(),
+    secret: 'qpoweirufjk148u30rewiojfdsklmx0854uweroijdsflkmv'
+}))
+
+app.get('/', (req, res) => {
+    res.render('index', {locals: {title: 'Welcome'}});
+});
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on PORT: ${PORT}`)
+});
